8fc8e6cf4496a8f5984fbd346e5114968d85eb11
Artem Kozlovski <61842660+temakazik@users.noreply.github.com>
2023-04-05 17:01:08 +0300

    Add extension methods

--- CryptoExchange/Microservices/UserManagement/UserManagement.Api/UserManagement.Api/Program.cs
+++ CryptoExchange/Microservices/UserManagement/UserManagement.Api/UserManagement.Api/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.Text;
+using UserManagement.Api.ExtensionMethods;
 using UserManagement.Core.Models;
 using UserManagement.Core.Repositories;
 using UserManagement.Core.Security;
@@ -15,25 +16,8 @@ using UserManagement.Services.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddAuthentication(k =>
-{
-    k.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    k.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(p =>
-{
-    var key = Encoding.UTF8.GetBytes(builder.Configuration["JWTToken:key"]);
-    p.SaveToken = true;
-    p.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = false,
-        ValidateAudience = false,
-        ValidateLifetime = false,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = builder.Configuration["JWKToekn:key"],
-        ValidAudience = builder.Configuration["JWKToekn:Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(key)
-    };
-});
+
+builder.Services.AddJWTAuthentication(builder.Configuration);
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
@@ -55,48 +39,12 @@ builder.Services.AddScoped<IRegistrationService, RegistrationService>();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
-builder.Services.AddControllers()
-    .AddNewtonsoftJson(options =>
-    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
-);
+builder.Services.AddControllersWithJsonConfiguration();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc(
-        "v1",
-        new OpenApiInfo
-        {
-            Title = "UserManagement API",
-            Version = "v1"
-        });
-
-    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        In = ParameterLocation.Header,
-        Description = "Please enter a valid token",
-        Name = "Authorization",
-        Type = SecuritySchemeType.Http,
-        BearerFormat = "JWT",
-        Scheme = "Bearer"
-    });
-    options.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type=ReferenceType.SecurityScheme,
-                    Id="Bearer"
-                }
-            },
-            new string[]{}
-        }
-    });
-});
+builder.Services.ConfigureSwagger();
 
 var app = builder.Build();
 
