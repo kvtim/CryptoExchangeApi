2accadbd24d7b83c4b245a174844a7d0846a4b14
Artem Kozlovski <61842660+temakazik@users.noreply.github.com>
2023-04-10 14:11:41 +0300

    Add name validation

--- CryptoExchange/Microservices/UserManagement/UserManagement.Core/Dtos/User/RegisterUserDto.cs
+++ CryptoExchange/Microservices/UserManagement/UserManagement.Core/Dtos/User/RegisterUserDto.cs
@@ -11,9 +11,11 @@ namespace UserManagement.Core.Dtos.User
     public class RegisterUserDto
     {
         [MinLength(2), MaxLength(50)]
+        [NameValidation]
         public required string? FirstName { get; set; }
 
         [MinLength(2), MaxLength(100)]
+        [NameValidation]
         public required string? LastName { get; set; }
 
         [EmailAddress]
--- CryptoExchange/Microservices/UserManagement/UserManagement.Core/Dtos/User/UpdateUserDto.cs
+++ CryptoExchange/Microservices/UserManagement/UserManagement.Core/Dtos/User/UpdateUserDto.cs
@@ -5,15 +5,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UserManagement.Core.Models;
+using UserManagement.Core.Validation;
 
 namespace UserManagement.Core.Dtos.User
 {
     public class UpdateUserDto
     {
         [MinLength(2), MaxLength(50)]
+        [NameValidation]
         public required string? FirstName { get; set; }
 
         [MinLength(2), MaxLength(100)]
+        [NameValidation]
         public required string? LastName { get; set; }
     }
 }
