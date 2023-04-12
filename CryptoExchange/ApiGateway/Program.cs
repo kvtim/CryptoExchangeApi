4d088cb6cd6f1c42adffed363d2982db99eba408
Artem Kozlovski <61842660+temakazik@users.noreply.github.com>
2023-04-12 15:33:38 +0300

    Configure swagger for Api Gateway

--- CryptoExchange/ApiGateway/Program.cs
+++ CryptoExchange/ApiGateway/Program.cs
@@ -11,18 +11,21 @@ builder.Services.AddCors(options => {
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot(builder.Configuration);
+
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerForOcelotUI(opt =>
+    {
+        opt.PathToSwaggerGenerator = "/swagger/docs";
+    });
 }
 
 app.UseCors("CORSPolicy");
