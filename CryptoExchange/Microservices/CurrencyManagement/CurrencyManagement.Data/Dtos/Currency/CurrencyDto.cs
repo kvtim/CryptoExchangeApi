f86a00c043c70fe10dae77149049375443fd59c4
Artem Kozlovski <61842660+temakazik@users.noreply.github.com>
2023-04-13 16:12:00 +0300

    Update dtos

--- CryptoExchange/Microservices/CurrencyManagement/CurrencyManagement.Data/Dtos/Currency/CurrencyDto.cs
+++ CryptoExchange/Microservices/CurrencyManagement/CurrencyManagement.Data/Dtos/Currency/CurrencyDto.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using CurrencyManagement.Core.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,5 +13,6 @@ namespace CurrencyManagement.Data.Dtos.Currency
         public int Id { get; set; }
         public string? Name { get; set; }
         public decimal CurrentPriceInUSD { get; set; }
+        public CurrencyType CurrencyType { get; set; }
     }
 }
